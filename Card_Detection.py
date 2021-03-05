ddc53123f310e4452686e878c9ab029affee4d11
Minkwon Kim <rulesky_kim@snu.ac.kr>
2021-03-06 02:12:42 +0900

    Add: Image from Camera

--- Card_Detection.py
+++ Card_Detection.py
@@ -6,7 +6,7 @@ def getContours(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area>500:
+        if area>2000:
             print(area)
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
             peri = cv2.arcLength(cnt, True)
@@ -43,21 +43,28 @@ def getContours(img):
             cv2.putText(imgContour, objectType,
                         (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
 
-path = 'Resources/card.jpg'
-img = cv2.imread(path)
-imgContour = img.copy()
 
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
-imgCanny = cv2.Canny(imgBlur, 50, 50)
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 780)
+cap.set(10, 100)
 
 while True:
+    success, img = cap.read()
+    # path = 'Resources/card_L.jpg'
+    # img = cv2.imread(path)
+    imgContour = img.copy()
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+    imgCanny = cv2.Canny(imgBlur, 50, 50)
 
     getContours(imgCanny)
 
     # cv2.imshow("Original", img)
     # cv2.imshow("Gray", imgGray)
     # cv2.imshow("Blur", imgBlur)
-    cv2.imshow("Canny", imgCanny)
+    # cv2.imshow("Video", img)
+    # cv2.imshow("Canny", imgCanny)
     cv2.imshow("Contour", imgContour)
     cv2.waitKey(1)
